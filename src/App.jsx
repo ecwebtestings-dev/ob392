f67f5f0ce3611d64256f4ded40e3a861767b5940
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-07-15 21:08:35 +0300

    Add missing files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
-
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
-import ContactPage from "./pages/Contact/Contact";
 
 
 
@@ -15,7 +13,6 @@ function App() {
         <Route element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactPage />} />
           
           
         </Route>
