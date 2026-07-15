abe8ebba5fd211fd99e40ca97e4806c799c42d12
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-07-15 21:30:23 +0300

    Updates3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Services from "./pages/Services/Services";
+import ContactPage from "./pages/Contact/Contact";
 
 
 
@@ -13,7 +15,8 @@ function App() {
         <Route element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          
+          <Route path="/services" element={<Services/>}/>
+          <Route path="/contact" element={<ContactPage/>}/>
           
         </Route>
       </Routes>
