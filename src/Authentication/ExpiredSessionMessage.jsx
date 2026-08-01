903cb90575484c6b2f1bb461ae986a7e5c791ee3
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-08-01 17:54:20 +0300

    Update

--- src/Authentication/ExpiredSessionMessage.jsx
+++ src/Authentication/ExpiredSessionMessage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
-import { onSessionExpired } from "./SessionEvent";
+import { onSessionExpired } from "./SessionEvents";
 import { api } from "./api";
 
 
