e85ee84360a00692c10200c1db69e822d0fa7fe9
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-08-01 18:21:37 +0300

    Last commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import Inquiries from "./Dashboard/Inquires/InQuiries";
 import { UsersTable } from "./Dashboard/UserManagement/userManagement";
 import DashboardOverview from "./Dashboard/Statatics/DashboardOverview";
 import { AuditLogsTable } from "./Dashboard/AuditLogs/AuditLogsTable";
-import SessionExpiredModal from "./Authentication/expiredSessionMessage";
+import SessionExpiredModal from "./Authentication/ExpiredSessionMessage";
 //import SearchResults from "./Dashboard/GlobalSearch/SearchResults";
 
 
@@ -66,7 +66,7 @@ function App() {
 
     <BrowserRouter>
     {/* fired from Authentication/api.js and shows itself when triggered. */}
-      <SessionExpiredModal />
+      <SessionExpiredModal
       
       <Routes>
         <Route element={<MainLayout />}>
