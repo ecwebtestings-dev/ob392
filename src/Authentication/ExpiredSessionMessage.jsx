778fb1ed2893c413ab681a236636dad155c968c6
ecwebtestings-dev <ecwebtestings@gmail.com>
2026-08-01 17:51:18 +0300

    SESSIOEN EROR 2

--- src/Authentication/ExpiredSessionMessage.jsx
+++ src/Authentication/ExpiredSessionMessage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
-import { onSessionExpired } from "./sessionEvents";
+import { onSessionExpired } from "./SessionEvent";
 import { api } from "./api";
 
 
--- src/Authentication/api.js
+++ src/Authentication/api.js
@@ -1,4 +1,4 @@
-import { emitSessionExpired } from "./sessionEvents";
+import { emitSessionExpired } from "./SessionEvents";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
